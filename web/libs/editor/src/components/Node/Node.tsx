05c697b7ff460628a6b20fc6e2b3ef0b6e6e8ff5
hlomzik <hlomzik@gmail.com>
2024-11-05 20:16:55 +0000

    Use better icon for timeline regions

--- web/libs/editor/src/components/Node/Node.tsx
+++ web/libs/editor/src/components/Node/Node.tsx
@@ -21,7 +21,7 @@ import {
   IconText,
   IconWarning,
 } from "../../assets/icons";
-import { IconFullscreen } from "../../assets/icons/timeline";
+import { IconTimelineRegion } from "../../assets/icons/timeline";
 import { NodeView } from "./NodeView";
 import { Tooltip } from "../../common/Tooltip/Tooltip";
 
@@ -118,7 +118,7 @@ const NodeViews = {
 
   TimelineRegionModel: NodeView({
     name: "Timeline Span",
-    icon: IconFullscreen,
+    icon: IconTimelineRegion,
   }),
 };
 

6d672b96d7407c5df76295b91779217e22889c80
hlomzik <hlomzik@gmail.com>
2024-11-18 15:03:59 +0000

    Update Sentry init to newer versions

--- web/apps/labelstudio/src/config/Sentry.ts
+++ web/apps/labelstudio/src/config/Sentry.ts
@@ -1,7 +1,6 @@
 import * as Sentry from "@sentry/browser";
 import * as ReactSentry from "@sentry/react";
-import type { RouterHistory } from "@sentry/react/dist/reactrouter";
-import { Integrations } from "@sentry/tracing";
+import type { RouterHistory } from "@sentry/react/build/types/reactrouter";
 import { Route } from "react-router-dom";
 
 export const initSentry = (history: RouterHistory) => {
@@ -9,11 +8,7 @@ export const initSentry = (history: RouterHistory) => {
     setTags();
     Sentry.init({
       dsn: "https://5f51920ff82a4675a495870244869c6b@o227124.ingest.sentry.io/5838868",
-      integrations: [
-        new Integrations.BrowserTracing({
-          routingInstrumentation: ReactSentry.reactRouterV5Instrumentation(history),
-        }),
-      ],
+      integrations: [Sentry.browserTracingIntegration(), ReactSentry.reactRouterV5BrowserTracingIntegration({ history })],
       environment: process.env.NODE_ENV,
       // Set tracesSampleRate to 1.0 to capture 100%
       // of transactions for performance monitoring.

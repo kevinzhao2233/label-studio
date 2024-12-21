bb147e9c2b9a59f8df4b1cd994bcf769bc852d96
Brandon Martel <brandonmartel@gmail.com>
2024-12-20 19:19:53 -0600

    testing a theory, revert this later

--- label_studio/core/utils/manifest_assets.py
+++ label_studio/core/utils/manifest_assets.py
@@ -6,11 +6,10 @@ from django.conf import settings
 # Load manifest.json once at module scope
 _MANIFEST = {}
 try:
-    if not settings.DEBUG:
-        manifest_path = Path(settings.STATIC_ROOT) / 'js/manifest.json'
-        if manifest_path.exists():
-            with open(manifest_path, 'r') as f:
-                _MANIFEST = json.load(f)
+    manifest_path = Path(settings.STATIC_ROOT) / 'js/manifest.json'
+    if manifest_path.exists():
+        with open(manifest_path, 'r') as f:
+            _MANIFEST = json.load(f)
 except Exception:
     # If there's any error reading the manifest, we'll use the default mapping
     pass

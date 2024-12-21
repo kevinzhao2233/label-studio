9b6e363bcbcbc0ad3bc7b146ff1e5260c43200ff
Brandon Martel <brandonmartel@gmail.com>
2024-12-20 19:43:14 -0600

    properly handle manifest assets to only load when not in HMR mode

--- label_studio/core/utils/manifest_assets.py
+++ label_studio/core/utils/manifest_assets.py
@@ -6,10 +6,13 @@ from django.conf import settings
 # Load manifest.json once at module scope
 _MANIFEST = {}
 try:
-    manifest_path = Path(settings.STATIC_ROOT) / 'js/manifest.json'
-    if manifest_path.exists():
-        with open(manifest_path, 'r') as f:
-            _MANIFEST = json.load(f)
+    # If HMR is enabled, we don't need to read the manifest as it's not used
+    # All assets are served from the webpack dev server in that case
+    if not settings.FRONTEND_HMR:
+        manifest_path = Path(settings.STATIC_ROOT) / 'js/manifest.json'
+        if manifest_path.exists():
+            with open(manifest_path, 'r') as f:
+                _MANIFEST = json.load(f)
 except Exception:
     # If there's any error reading the manifest, we'll use the default mapping
     pass

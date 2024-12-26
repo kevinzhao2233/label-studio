5e79486f909cd3d215142c144bf92af4f6ab4f88
Marcel Canu <marcel@humansignal.com>
2024-12-26 15:57:39 -0300

    Tracking instance install

--- label_studio/core/context_processors.py
+++ label_studio/core/context_processors.py
@@ -1,8 +1,13 @@
 """This file and its contents are licensed under the Apache License 2.0. Please see the included NOTICE for copyright information and LICENSE for a copy of the license.
 """
+import os
+
 from core.feature_flags import all_flags
 from core.utils.common import collect_versions
 from django.conf import settings as django_settings
+from django.utils import timezone
+
+from label_studio.core.utils.io import get_config_dir
 
 
 def sentry_fe(request):
@@ -33,3 +38,33 @@ def settings(request):
         feature_flags = all_flags(request.user)
 
     return {'settings': django_settings, 'versions': versions, 'feature_flags': feature_flags}
+
+
+def frontend_events(request):
+    events = []
+    if should_send_install_event():
+        events.append(
+            {
+                'name': 'setup.install',
+                'with_iframe': True,
+            }
+        )
+    return {'frontend_events': events}
+
+
+_INSTALL_EVENT_SENT = False
+
+
+def should_send_install_event():
+    # Only fire install event once per instance
+    global _INSTALL_EVENT_SENT
+    if django_settings.VERSION_EDITION == 'Community' and not _INSTALL_EVENT_SENT:
+        install_file = os.path.join(get_config_dir(), 'install.txt')
+        if not os.path.exists(install_file):
+            with open(install_file, 'w') as f:
+                f.write(timezone.now().isoformat())
+            _INSTALL_EVENT_SENT = True
+            return True
+        _INSTALL_EVENT_SENT = True
+
+    return False
--- web/apps/labelstudio/src/app/RootPage.jsx
+++ web/apps/labelstudio/src/app/RootPage.jsx
@@ -1,11 +1,29 @@
 import { Menubar } from "../components/Menubar/Menubar";
 import { ProjectRoutes } from "../routes/ProjectRoutes";
 import { useOrgValidation } from "../hooks/useOrgValidation";
+import { useEffect } from "react";
 
 export const RootPage = ({ content }) => {
   useOrgValidation();
   const pinned = localStorage.getItem("sidebar-pinned") === "true";
   const opened = pinned && localStorage.getItem("sidebar-opened") === "true";
+
+  useEffect(() => {
+    const frontendEvents = window.APP_SETTINGS.frontend_events;
+
+    frontendEvents.forEach((event) => {
+      if (window.APP_SETTINGS.collect_analytics && event.with_iframe) {
+        const iframe = document.createElement("iframe");
+        iframe.src = `https://www.labelstud.io/track/?event=${event.name}`;
+        iframe.style.display = "none";
+        iframe.style.visibility = "hidden";
+        document.body.appendChild(iframe);
+      }
+
+      // TODO: __lsa(event.name)
+      // I don't know why but __lsa isn't defined yet here
+    });
+  }, []);
 
   return (
     <Menubar

88fac17441d5be0e61616da59329827ed4ebe7f1
Raul Martin <raul@humansignal.com>
2025-01-08 13:58:25 -0800

    refactor: OPTIC-1506: Remove Stale Feature Flag - fflag_fix_front_dev_3350_restrict_drawing_area_short

--- label_studio/core/feature_flags/stale_feature_flags.py
+++ label_studio/core/feature_flags/stale_feature_flags.py
@@ -31,7 +31,6 @@ STALE_FEATURE_FLAGS = {
     'fflag_feat_back_dev_3756_queue_enrollment_min_short': False,
     'fflag_fix_front_dev_3666_max_usages_on_region_creation_171122_short': True,
     'fflag_fix_front_dev_3617_taxonomy_memory_leaks_fix': True,
-    'fflag_fix_front_dev_3350_restrict_drawing_area_short': True,
     'fflag_feat_front_dev_2461_audio_paragraphs_seek_chunk_position_short': True,
     'ff_front_dev_2432_auto_save_polygon_draft_210622_short': True,
     'ff_front_dev_2575_projects_list_performance_280622_short': True,

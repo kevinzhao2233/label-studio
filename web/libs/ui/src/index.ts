5b78fd6c626dc7a54d956d44cb5e3f396f15731e
Brandon Martel <brandonmartel@gmail.com>
2025-01-15 08:01:50 -0600

    remove the moved Tour related code from UI export

--- web/libs/ui/src/index.ts
+++ web/libs/ui/src/index.ts
@@ -2,7 +2,5 @@ export * from "./lib/checkbox/checkbox";
 export * from "./lib/Toast/Toast";
 export * from "./lib/label/label";
 export * from "./lib/toggle/toggle";
-export * from "./lib/Tour/Tour";
-export * from "../../../apps/labelstudio/src/providers/TourProvider";
 
 export * from "./assets/icons";

4dff6d04211bb96ba114fca9902b2413b749b71f
nik <nik@heartex.net>
2025-01-29 09:55:31 -0800

    Fix tests

--- label_studio/tests/sdk/test_export.py
+++ label_studio/tests/sdk/test_export.py
@@ -1,5 +1,6 @@
-import pytest
 import pandas as pd
+import pytest
+
 from label_studio.tests.sdk.common import LABEL_CONFIG_AND_TASKS
 
 pytestmark = pytest.mark.django_db
@@ -16,20 +17,20 @@ def test_project(django_live_url, business_client):
 
 def test_export_formats(test_project):
     ls, project = test_project
-    
+
     # Get available export formats
-    formats = ls.projects.exports.get_formats(project.id)
+    formats = ls.projects.exports.list_formats(project.id)
     assert len(formats) > 0
 
 
 def test_direct_export(test_project):
     ls, project = test_project
-    
+
     # Test JSON export
     json_data = ls.projects.exports.as_json(project.id)
     assert isinstance(json_data, list)
     assert len(json_data) == 1
-    
+
     # Test pandas export
     df = ls.projects.exports.as_pandas(project.id)
     assert isinstance(df, pd.DataFrame)

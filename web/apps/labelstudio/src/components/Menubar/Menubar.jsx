dbc22f90f854c8882e5a46d42368e63045d55a5c
Nicholaus (Nico) Halecky <nico@humansignal.com>
2025-01-31 16:44:00 -0300

    Revert "feat: DIA-1794: [FE] Implement home page in LSO and LSE (#6885)"
    This reverts commit e95280e1d4731af31047009a25eebf237964f435.

--- web/apps/labelstudio/src/components/Menubar/Menubar.jsx
+++ web/apps/labelstudio/src/components/Menubar/Menubar.jsx
@@ -26,8 +26,7 @@ import "./Menubar.scss";
 import "./MenuContent.scss";
 import "./MenuSidebar.scss";
 import { ModelsPage } from "../../pages/Organization/Models/ModelsPage";
-import { FF_DIA_835, FF_HOMEPAGE, isFF } from "../../utils/feature-flags";
-import { IconHome } from "@humansignal/ui";
+import { FF_DIA_835, isFF } from "../../utils/feature-flags";
 
 export const MenubarContext = createContext();
 
@@ -184,7 +183,6 @@ export const Menubar = ({ enabled, defaultOpened, defaultPinned, children, onSid
               style={{ width: 240 }}
             >
               <Menu>
-                {isFF(FF_HOMEPAGE) && <Menu.Item label="Home" to="/" icon={<IconHome />} data-external exact />}
                 <Menu.Item label="Projects" to="/projects" icon={<IconFolder />} data-external exact />
                 <Menu.Item label="Organization" to="/organization" icon={<IconPersonInCircle />} data-external exact />
                 {isFF(FF_DIA_835) && <Menu.Item label="Models" to={ModelsPage.path} icon={<IconModel />} exact />}

7df9ed946f32acbaf3db97b6789d35a375a5bfc3
pakelley <pakelley@hey.com>
2025-02-05 13:30:27 -0800

    add missing serializer

--- label_studio/jwt_auth/serializers.py
+++ label_studio/jwt_auth/serializers.py
@@ -1,7 +1,30 @@
 from datetime import datetime
 
-from jwt_auth.models import LSAPIToken
+from jwt_auth.models import JWTSettings, LSAPIToken
 from rest_framework import serializers
+
+
+# Recommended implementation from JWT to support drf-yasg:
+# https://django-rest-framework-simplejwt.readthedocs.io/en/latest/drf_yasg_integration.html
+class TokenRefreshResponseSerializer(serializers.Serializer):
+    access = serializers.CharField()
+
+    # TODO do we really need these NotImplementedErrors?
+    def create(self, validated_data):
+        raise NotImplementedError()
+
+    def update(self, instance, validated_data):
+        raise NotImplementedError()
+
+
+class JWTSettingsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = JWTSettings
+        fields = ('enabled', 'ttl_days')
+
+
+class JWTSettingsUpdateSerializer(JWTSettingsSerializer):
+    pass
 
 
 class LSAPITokenCreateSerializer(serializers.Serializer):
@@ -21,6 +44,7 @@ class LSAPITokenCreateSerializer(serializers.Serializer):
     class Meta:
         model = LSAPIToken
         fields = ['token']
+
 
 class LSAPITokenListSerializer(LSAPITokenCreateSerializer):
 

1cab918b5f0686a85703a7773eaef03b35b36ba0
Nick Skriabin <nr@fenelon.ru>
2025-02-21 16:06:02 +0000

    Remove excess

--- web/libs/core/src/index.ts
+++ web/libs/core/src/index.ts
@@ -1,8 +1,7 @@
 import * as ff from "./lib/utils/feature-flags";
 import * as pages from "./pages";
-import * as utils from "./lib/utils";
 
 export * from "./lib/Tour";
 export * from "./lib/utils/analytics";
 
-export { ff, pages, utils };
+export { ff, pages };

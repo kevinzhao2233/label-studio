7a2babb7dc93d23578b53039158263e0cd05d621
Marcel Canu <marcel@humansignal.com>
2025-02-25 17:13:33 -0300

    Using create_organization

--- label_studio/organizations/tests/factories.py
+++ label_studio/organizations/tests/factories.py
@@ -1,5 +1,5 @@
 import factory
-from organizations.models import Organization, OrganizationMember
+from organizations.models import Organization
 
 
 class OrganizationFactory(factory.django.DjangoModelFactory):
@@ -9,11 +9,13 @@ class OrganizationFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Organization
 
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        return Organization.create_organization(**kwargs)
+
     @factory.post_generation
-    def created_by_relationship(self, create, extracted, **kwargs):
+    def created_by_active_organization(self, create, extracted, **kwargs):
         if not create or not self.created_by:
             return
-
         self.created_by.active_organization = self
         self.created_by.save(update_fields=['active_organization'])
-        OrganizationMember.objects.create(user=self.created_by, organization=self)
